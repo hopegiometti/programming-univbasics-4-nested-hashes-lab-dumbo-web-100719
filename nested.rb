8d949389d27e04a3e912fdad794ed62e0b3892ed
Hope Giometti <hopegiometti@gmail.com>
2019-09-20 19:01:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,11 +15,6 @@ def hopper
           :languages => ["C"]
         }
      }
-		 programmer_hash[:yukihiro_matsumoto] = {
-		 	:known_for => "Ruby",
-		 	:languages => ["LISP", "C"]
-		 	}
-			return programmer_hash
 end
 
 def alan_kay_is_known_for
