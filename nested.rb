d4064c2ecb0c7c2797d44e33714ef4b88783fb32
Hope Giometti <hopegiometti@gmail.com>
2019-09-20 18:56:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,8 +80,8 @@ def adding_matz
      }
 
   programmer_hash[:yukihiro_matsumoto] = {
-    :known_for => "COBOL",
-    :languages => ["COBOL", "FORTRAN"]
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
     }
 
 programmer_hash
