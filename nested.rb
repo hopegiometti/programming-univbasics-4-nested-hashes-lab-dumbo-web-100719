c0c5e23702592340513cdb30fc4bdcf2e61c34c3
Hope Giometti <hopegiometti@gmail.com>
2019-09-20 19:07:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,7 +78,10 @@ def adding_matz
         }
      }
 
-		 programmer_hash[:yukihiro_matsumoto] ={:known_for => "Ruby",:languages => ["LISP, C"]}
+		 programmer_hash[:yukihiro_matsumoto] = {
+			 :known_for => "Ruby",
+			 :languages => ["LISP, C"]
+		 }
 		 #  {:known_for => "Ruby",:languages => ["LISP, C"]}
 		   programmer_hash
 end
